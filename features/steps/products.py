26cce2e583663202e2d8d695f17d6b477c137323
salah zemmouri <sszemmouri@gmail.com>
2024-05-31 14:59:46 +0200

    [steps] add necessary steps for scenario to add product in cart

--- features/steps/products.py
+++ features/steps/products.py
@@ -1,18 +1,21 @@
-from behave import then
+from behave import when, then
 
 
-@When('he adds "{product_pretty_name}" product to cart')
+@when('he adds "{product_pretty_name}" product to cart')
 def step_impl(context, product_pretty_name):
+    context.page.product_tiles(product_pretty_name).add_to_cart()
 
 
 @then('he should be on sauce lab products page')
 def step_impl(context):
     context.page.page_is_visible()
 
+
 @then('the add to cart button turns into remove button')
 def step_impl(context):
+    pass
 
 
 @then('the cart should contain "{number_products_in_cart}" product')
 def step_impl(context, number_products_in_cart):
-
+    pass
