cbfbf1a69aa7302c6997d6376deb4d5051d36cd9
salah zemmouri <sszemmouri@gmail.com>
2024-06-03 10:35:41 +0200

    [components][page_objects] rework component factory

--- components/component_factory.py
+++ components/component_factory.py
@@ -10,17 +10,32 @@ class ComponentFactory:
     def __init__(self, driver):
         self._driver = driver
 
-    def create_footer(self):
+    def __call__(self, component_name, *args, **kwargs):
+        component_name_to_function_creation = {
+            'footer': self._create_footer,
+            'primary header': self._create_primary_header,
+            'secondary header': self._create_secondary_header,
+            'product tile': self._create_product_tile,
+            'inventory container': self._create_inventory_container,
+        }
+        component_method = component_name_to_function_creation.get(
+            component_name)
+        if component_method:
+            return component_method(*args, **kwargs) if args or kwargs else component_method()
+        else:
+            raise ValueError(f"Component {component_name} not found")
+
+    def _create_footer(self):
         return Footer(self._driver)
 
-    def create_inventory_container(self):
+    def _create_inventory_container(self):
         return InventoryContainer(self._driver)
 
-    def create_primary_header(self):
+    def _create_primary_header(self):
         return PrimaryHeader(self._driver)
 
-    def create_product_tile(self, product_pretty_name):
+    def _create_product_tile(self, product_pretty_name):
         return ProductTile(self._driver, product_pretty_name)
 
-    def create_secondary_header(self):
+    def _create_secondary_header(self):
         return SecondaryHeader(self._driver)
--- page_object/products_page.py
+++ page_object/products_page.py
@@ -1,4 +1,3 @@
-from components.product_tile import ProductTile
 from page_object.base_page import BasePage
 
 
@@ -6,15 +5,14 @@ class ProductsPage(BasePage):
 
     def __init__(self, driver):
         super().__init__(driver)
-        self.footer = self.component_factory.create_footer()
+        self.footer = self.component_factory("footer")
         self._products_tiles = {}
-        self.primary_header = self.component_factory.create_primary_header()
-        self.secondary_header = self.component_factory\
-            .create_secondary_header()
+        self.primary_header = self.component_factory("primary header")
+        self.secondary_header = self.component_factory("secondary header")
 
     def page_is_visible(self):
-        product_tile = self.component_factory.create_product_tile(
-            "Sauce Labs Backpack")
+        product_tile = self.component_factory(
+            "product tile", "Sauce Labs Backpack")
         return all((self.primary_header.is_visible(),
                     self.secondary_header.is_visible(),
                     product_tile.is_visible(),
@@ -24,5 +22,5 @@ class ProductsPage(BasePage):
     def product_tiles(self, product_pretty_name):
         if product_pretty_name not in self._products_tiles:
             self._products_tiles[product_pretty_name] = self\
-                .component_factory.create_product_tile(product_pretty_name)
+                .component_factory("product tile", product_pretty_name)
         return self._products_tiles[product_pretty_name]
