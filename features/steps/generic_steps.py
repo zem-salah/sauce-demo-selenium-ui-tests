b1b3d969c6e54cc23b799a9cd83604e4ed62c877
salah zemmouri <sszemmouri@gmail.com>
2024-06-07 16:09:46 +0200

    [steps] add generic steps file and add step to check user is on specific page

---
+++ features/steps/generic_steps.py
@@ -0,0 +1,15 @@
+from behave import then, when
+from robber import expect
+
+from page_object.page_factory import PageFactory
+
+"""
+This file contains steps that are low level and common to all pages.
+"""
+
+
+@then('he should be on "{page_pretty_name}" page')
+def step_impl(context, page_pretty_name):
+    expected_page = PageFactory(context.driver)(page_pretty_name)
+    expect(isinstance(context.page, type(expected_page))).to.be.true()
+    context.page.page_is_visible()
