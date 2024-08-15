715257e3ebef6177f520d7b6a0b1903218b10aaf
salah zemmouri <sszemmouri@gmail.com>
2024-08-15 16:22:49 +0200

    [page_objects] add checkout overview page object

--- page_object/page_factory.py
+++ page_object/page_factory.py
@@ -1,5 +1,6 @@
 from page_object.cart_page import CartPage
 from page_object.checkout_information_page import CheckoutInformationPage
+from page_object.checkout_overview_page import CheckoutOverviewPage
 from page_object.login import Login
 from page_object.products_page import ProductsPage
 
@@ -15,6 +16,7 @@ class PageFactory:
             'products': self._create_products_page,
             'your cart': self._create_your_cart_page,
             'checkout information': self._create_checkout_information_page,
+            'checkout overview': self._create_checkout_overview_page,
         }
         page_method = page_name_to_creation_function.get(page_name)
         if page_method:
@@ -24,6 +26,9 @@ class PageFactory:
 
     def _create_checkout_information_page(self):
         return CheckoutInformationPage(self._driver)
+
+    def _create_checkout_overview_page(self):
+        return CheckoutOverviewPage(self._driver)
 
     def _create_login_page(self):
         return Login(self._driver)
