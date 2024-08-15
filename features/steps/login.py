b0c2f16df160c098f37d0141a0148fd695de2d56
salah zemmouri <sszemmouri@gmail.com>
2024-08-15 16:16:54 +0200

    [steps] refactor login via cookie and add comments

--- features/steps/login.py
+++ features/steps/login.py
@@ -16,11 +16,20 @@ def step_impl(context):
 @given('"{user_pretty_name}" is logged in and is on the "{page_pretty_name}" '
        'page')
 def step_impl(context, user_pretty_name, page_pretty_name):
+    """
+    Access a page directly via URL using cookies to authenticate.
+    :param user_pretty_name: User pretty name as defined in data/user
+    :param page_pretty_name: Page pretty name as defined in
+    page_object/page_factory
+    :return:
+    """
     context.page = PageFactory(context.driver)(page_pretty_name)
     context.driver.get(context.page.BASE_URL)
     context.current_user = User(user_pretty_name)
-    context.page.navigate(cookie_name='session-username',
-                          cookie_value=context.current_user.name)
+    context.page.navigate(
+        cookie_name='session-username',
+        cookie_value=context.current_user.name,
+    )
     context.execute_steps(
         f'Then he should be on "{page_pretty_name}" page'
     )
@@ -28,6 +37,11 @@ def step_impl(context, user_pretty_name, page_pretty_name):
 
 @when('"{user_pretty_name}" logs in')
 def step_impl(context, user_pretty_name):
+    """
+    Login via UI
+    :param user_pretty_name:
+    :return:
+    """
     context.current_user = User(user_pretty_name)
     context.page = context.page.login(
         context.current_user.name, context.current_user.password)
